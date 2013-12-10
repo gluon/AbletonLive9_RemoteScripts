bd6c3968eb3790990d1e441276e631d46cf8eaee
Marat Bakirov <mbakirov@hotmail.com>
2013-12-11 02:36:11 +1100

    added bat file

---
+++ Novation_Impulse2/deploy.bat
@@ -0,0 +1,2 @@
+del /Q "D:\ProgramData\Ableton\Live 9 Suite\Resources\MIDI Remote Scripts\Novation_Impulse\*.*" 
+xcopy *.py "D:\ProgramData\Ableton\Live 9 Suite\Resources\MIDI Remote Scripts\Novation_Impulse\*.*" 
